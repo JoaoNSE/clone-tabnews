40600943c4688c892470ad8680836cc50309671b
João Henrique <nobreenrique@hotmail.com>
2023-10-17 01:58:28 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para treinar programação, originado do https://curso.dev do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Hello Warld</h1>
+  return <h1>Hello Warld</h1>;
 }
 
 export default Home;
